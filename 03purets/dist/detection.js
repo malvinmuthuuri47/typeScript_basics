a3e927f042850f1657f336f456e3cf4d19f3514e
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-11-18 23:09:15 +0300

    Final code for Type Narrowing

--- 03purets/dist/detection.js
+++ 03purets/dist/detection.js
@@ -5,3 +5,65 @@ function detectType(val) {
     }
     return val + 3;
 }
+function provideId(id) {
+    if (!id) {
+        console.log("Please provide ID");
+        return;
+    }
+    id.toLowerCase();
+}
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+}
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
+function getTrueShape(shape) {
+    if (shape.kind === "Circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "Circle":
+            return Math.PI * shape.radius ** 2;
+        case "Square":
+            return shape.side * shape.side;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
+}
